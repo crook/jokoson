296cdfcd88138632c590fcb7435635fba11b2b67
Ray Chen <chenrano2002@gmail.com>
2017-01-06 23:11:15 +0800

    Add docker compose file to help run multi-container app

---
+++ dev/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x 
+
+cd /code
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata ./jokoson/testfixtures/initial_data.json
+
+python manage.py runserver 0.0.0.0:8888
---
+++ dev/start.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -xe
+#
+# https://docs.docker.com/compose/
+
+#docker-compose up jokoson-db
+#docker-compose up jokoson-rest
+
+# Want to enter this container? Try this:
+# docker exec -it dev_jokoson-db_1 bash
+
+EXIST=`docker-compose ps -q`
+if [ "X" == "X${EXIST}" ]; then
+    docker-compose up -d
+else
+    docker-compose start
+fi
